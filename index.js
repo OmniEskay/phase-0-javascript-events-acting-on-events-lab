176bc6b278a1c40f9e6df7b042fb35d389366333
+ <omnieskay@proton.me>
2025-04-01 11:24:18 +0300

    acting on events

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left, 10) || 0;
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`; 
+    }
+}
+
+function moveDodgerRight() {
+    const left = parseInt(dodger.style.left, 10) || 0;
+    if (left < 360) { 
+        dodger.style.left = `${left + 10}px`; 
+    }
+}
